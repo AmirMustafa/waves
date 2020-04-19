65c46accf42bc798eaa7e1e9a452752704fb0100
Amir Mustafa <amirengg15@gmail.com>
2020-04-19 06:57:42 +0530

    Added { useNewUrlParser: true, useUnifiedTopology: true } extra parameter for mongoose connect

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const async = require("async");
 require("dotenv").config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
