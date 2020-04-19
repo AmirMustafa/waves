5ca806beb572016d2475172169f6963d29d61d0d
Amir Mustafa <amirengg15@gmail.com>
2020-04-19 08:01:30 +0530

    Added production code on server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const async = require("async");
 require("dotenv").config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE, {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -19,6 +19,8 @@ mongoose.connect(process.env.DATABASE, {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static("client/build")); // for production
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
@@ -452,6 +454,14 @@ app.post("/api/site/site_data", auth, admin, (req, res) => {
     }
   );
 });
+
+// DEFAULT
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.get("/*", (req, res) => {
+    res.sendfile(path.resolve(__dirname, "../client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
